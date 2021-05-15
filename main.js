01a4ded6c1074b0c86fd8099eaef29007ae46af1
shellyscheng <shellysc13@gmail.com>
2021-05-15 18:17:59 -0400

    updates

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ function log(message) {
 }
 var message = "Hello World";
 log(message);
+//in JS, the scope of var variable declared is the nearest function
+//tsc complied TS in ES5 JS
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log("Finally:" + i);
+}
+doSomething();
--- main.ts
+++ main.ts
@@ -7,3 +7,14 @@ log(message)
 
 
 //in JS, the scope of var variable declared is the nearest function
+//tsc complied TS in ES5 JS
+
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+
+    console.log("Finally:" + i);
+}
+
+doSomething();
